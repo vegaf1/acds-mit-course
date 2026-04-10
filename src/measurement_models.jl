255a7b0ad4348943ff0af1e1aff2af527ad64732
Fausto Vega <vegaf@mit.edu>
2026-04-09 21:42:03 -0400

    add in measurement models

--- src/measurement_models.jl
+++ src/measurement_models.jl
@@ -61,3 +61,218 @@ function create_inertial_measurements(Q_true, b_m, N_measurements)
     return i_m
 
 end
+
+
+#sun sensor model 
+
+"""
+additive sun sensor model
+"""
+#over some desired timeframe pass in both the satellite trajectory and sun trajectory at the desired sample rate
+#pass in postion trajectory
+#time trajectory is an array of epochs
+function generate_sun_measurements(satellite_trajectory, sun_trajectory, time_trajectory)
+
+    N = size(satellite_trajectory)[2]
+
+    sun_measurements = zeros(3, N)
+
+    for i=1:N
+        sun_position = sun_position(time_trajectory[i])
+
+        sun_measurements[:,i] = (sun_position - satellite_trajectory[:,i])/norm((sun_position - satellite_trajectory[:,i]))
+
+    end
+
+    return sun_measurements
+end
+
+
+function generate_noisy_sun_measurements(attitude_trajectory, ground_truth_measurments)
+
+
+    #misalignment matrix
+    M = [1 -1.74e-3 1.74e-3; 1.74e-3 1 -1.74e-3; -1.74e-3 1.74e-3 1] 
+    
+    #constant bias    
+    b = ones(3)*(8.72e-4)
+
+    #covariance matrix for the noise
+    W = Matrix(1.0*I,3,3)*(7.61e-5)
+
+    noisy_sun_measurements = zeros(3, N)
+
+    #Q function transforms a quaternion into a rotation matrix
+    for i=1:N
+        
+        noisy_sun_measurements[:,i] = M*(Q(attitude_trajectory[:,:,i])'*ground_truth_measurments[:,i]) + b + cholesky(W)*randn(3)
+
+    end
+
+    return noisy_sun_measurements
+
+end
+
+
+#over time, get the magnetometer measurement from IGRF 14 from SatelliteToolboxGeomagneticField.jl 
+#time trajectory is a set of epochs
+#satellite trajectory is in ECI  
+#ground truth measurements
+#units of nT 
+function generate_magnetometer_measurements(time_trajectory, satellite_trajectory)
+
+    N = size(time_trajectory)[1]
+
+    ground_truth_measurements = zeros(3, N)
+    
+    #get the measurmements along the entire trajectory    
+    for i = 1:N 
+
+        # get decimal date
+        date = caldate(time_trajectory[i])
+        year = date[1]
+        day = day_of_year(time_trajectory[i])
+        decimal_date = year + day / 365.2425
+
+        #transform from ECI to ECEF 
+        state_ecef = sECItoECEF(time_trajectory[i], satellite_trajectory[:,i])
+
+        #transform ECEF to geodetic. output is lon [1], lat [2], altitude [3]
+        state_geodetic = sECEFtoGEOD(state_ecef)
+
+        #use ecef state to get the IGRF 14 measurement 
+        #the time is a fractional year. 
+        #the output is in north-east down coordinate system (checkout toolbox docs for definition)
+        b_measurement_ned = igrfd(decimal_date, state_geodetic[3], state_geodetic[2], state_geodetic[1], Val(:geodetic))
+
+        b_measurement_ecef = ned_to_ecef(b_measurement_ned, state_geodetic[2], state_geodetic[1], state_geodetic[3])
+
+        #compute the rotation matrix to convert from ecef to eci 
+        R_eci_ecef = rECEFtoECI(time_trajectory[i])
+
+        #ground truth measurements are in the ECI frame
+        ground_truth_measurements[:,i] = R_eci_ecef*b_measurement_ecef
+
+    end
+
+    return ground_truth_measurements
+
+end
+
+#generate the magnetometer measurements 
+#these are in units of nanoTeslas
+#in the body frame
+#attitude trajectory is in quaternions
+function generate_noisy_magnetometer_measurements(attitude_trajectory, ground_truth_measurments)
+
+    N = size(attitude_trajectory)[1]
+
+    #from spec sheet
+
+    #misalignment matrix
+    M = [1 -1.74e-2 1.74e-2; 1.74e-2 1 -1.74e-2; -1.74e-2 1.74e-2 1] 
+    
+    #constant bias    
+    b = ones(3)*100
+
+    #covariance matrix for the noise
+    W = Matrix(1.0*I,3,3)*1600
+
+    #these are in the body frame
+    noisy_mag_measurements = zeros(3, N)
+
+    #Q function transforms a quaternion into a rotation matrix
+    for i = 1:N 
+
+        noisy_mag_measurements = M*(Q(attitude_trajectory[:,:,i])'*ground_truth_measurments[:,i]) + b + randn(3)*cholesky(W) 
+
+    end
+
+    return noisy_mag_measurements
+
+end
+
+
+#noisy star tracker measurements
+
+function generate_star_tracker_measurement(attitude_trajectory)
+
+    #number of star trackers
+    m = 2 
+
+    #4 arcseconds -> radians
+    σ_cross = 0.00001939254724438
+
+    #30 arcseconds -> radians
+    #these are from the blue canyon nano startracker data sheet
+    σ_roll = 0.0001454441043329
+
+    W = [σ_cross^2 0 0; 0 σ_cross^2 0; 0 0 σ_roll^2]
+
+    N = size(attitude_trajectory)[2]
+
+    star_tracker_measurements = zeros(3*m,N)
+
+    for i = 1:N 
+        
+        #sample random axis angle vector
+        ϕ_1 = randn(3)*sqrt(W)
+        ϕ_2 = randn(3)*sqrt(W)
+        
+        #apply it to the true quaternion
+
+        #left multiply true quaternion with delta quaternion
+        star_tracker_measurements[1:3,i] = L(attitude_trajectory[:,i])*expq(ϕ_1)
+        star_tracker_measurements[4:6,i] = L(attitude_trajectory[:,i])*expq(ϕ_2)
+
+    end 
+
+    return star_tracker_measurements
+
+end
+
+#these are already in the body frame
+#need to pass in the timestep because one of uncertainties is in units of sqrt(hr)...
+#ensure dt is in hours
+function generate_gyro_measurements(ω_true, bias_true, dt)
+
+    #1-sigma for gyro measurements
+    #in rad^2 per hr^2 
+    W_hr = [6.85e-4 0 0; 0 1.68e-3 0; 0 0 8.80e-4]
+
+    #convert to (rad/s)
+    W = W_hr/(3600^2)
+
+    #this is what is used to simulate the true gyro bias
+    #1 sigma standard deviation for bias 
+    #converted from degrees/sqrt(hr) to radians/sqrt(hr) 
+    b = [0.002268928, 0.002268928, 0.003316126]
+
+    #dt is passed in as an hour already 
+    b_rad_hr = b/sqrt(dt)
+
+    #convert rad/hr into rad/s 
+
+    b_rad_s = b_rad_hr*(1/3600)
+
+    #these are in radians per second
+    W_b = [b_rad_s[1]^2 0 0; 0 b_rad_s[2]^2 0; 0 0 b_rad_s[3]^2]
+
+    #misalignment matrix (in radians)
+    M = [1 -4.36e-3 4.36e-3; 4.36e-3 1 -4.36e-3; -4.36e-3 4.36e-3 1] 
+
+    N = size(ω_true)
+    
+    gyro_measurements = zeros(3, N)
+
+
+    for i=1:N
+
+        gyro_measurements[:,i] = ω_true[:,i] + bias_true[:,i] + randn(3)*cholesky(W)
+
+    end
+
+    return gyro_measurements
+
+end
+
